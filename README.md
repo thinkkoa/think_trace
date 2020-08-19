9e36c805d8ab2f6d4628be467733273589cd57d0
richenlin <richenlin@gmail.com>
2020-08-19 17:50:15 +0800

    Support more response content-type

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 -----
 
 [![npm version](https://badge.fury.io/js/think_trace.svg)](https://badge.fury.io/js/think_trace)
-[![Dependency Status](https://david-dm.org/thinkkoa/think_trace.svg)](https://david-dm.org/thinkkoa/think_trace)
 
 HTTP error intercept processing for ThinkKoa.
 
@@ -23,7 +22,7 @@ npm i think_trace
 config: { //中间件配置
     ...,
     trace: {
-        timeout: 30, //http请求超时时间,单位s
+        timeout: 30, //http服务超时时间,单位s
         error_code: 500, //报错时的状态码
         error_no_key: 'errno', //错误号的key
         error_msg_key: 'errmsg', //错误消息的key
--- index.js
+++ index.js
@@ -6,29 +6,190 @@
  * @version    17/4/29
  */
 const lib = require('think_lib');
-const httpError = require('http-errors');
 const logger = require('think_logger');
 
 /**
- * 渲染错误模板
  * 
+ * 
+ * @param {*} app
  * @param {*} ctx
  * @param {*} options
- * @param {*} err
+ * @param {*} body
  * @returns
  */
-const simpleRend = async function (ctx, options, err) {
-    if (ctx.compile) {
-        ctx._assign = Object.assign(ctx._assign || {}, {
-            [options.error_no_key || 'code']: ctx.status,
-            [options.error_msg_key || 'message']: err.message || ''
-        });
-        logger.info('auto render the error template.');
-        return ctx.compile(`${process.env.ROOT_PATH}${options.error_path}/${ctx.status}.html`, ctx._assign || {});
-    } else {
-        logger.warn('`think_view `middleware is not included, so it only outputs file content.');
-        return lib.readFile(`${process.env.ROOT_PATH}${options.error_path}/${ctx.status}.html`, 'utf-8');
+const htmlRend = async function (app, ctx, options, body) {
+    let contentType = 'text/html';
+    if (options.encoding !== false && contentType.indexOf('charset=') === -1) {
+        contentType = `${contentType}; charset=${options.encoding}`;
     }
+    ctx.type = contentType;
+    if (ctx.status < 400) {
+        ctx.body = body || ' ';
+        return null;
+    }
+    let res = '', stack = '';
+    let message = ctx.message;
+    if (lib.isError(body)) {
+        message = body.message;
+        stack = body.stack;
+    }
+    if (options.error_path) {
+        if (ctx.compile) {
+            ctx._assign = {
+                'status': ctx.status,
+                'message': message || ''
+            };
+            logger.info('auto render the error template.');
+            res = await ctx.compile(`${options.error_path}/${ctx.status}.html`, ctx._assign || {});
+        } else {
+            logger.warn('`think_view `middleware is not included, so it only outputs file content.');
+            res = await lib.readFile(`${options.error_path}/${ctx.status}.html`, 'utf-8');
+        }
+    } else {
+        res = `<!DOCTYPE html><html><head><title>Error - ${ctx.status}</title><meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0">
+            <style>body {padding: 50px 80px;font: 14px 'Microsoft YaHei','微软雅黑',Helvetica,Sans-serif;}h1, h2 {margin: 0;padding: 10px 0;}h1 {font-size: 2em;}h2 {font-size: 1.2em;font-weight: 200;color: #aaa;}pre {font-size: .8em;}</style>
+            </head><body><div id="error"><h1>Error</h1><p>Oops! Your visit is rejected!</p><h2>Message:</h2><pre><code>${message || ''}</code></pre>`;
+        // if (app.app_debug || body.expose) {
+        if (app.app_debug) {
+            res = `${res}<h2>Stack:</h2><pre><code>${stack || ''}</code></pre>`;
+        }
+        res = `${res}</div></body></html>`;
+    }
+    return ctx.res.end(res);
+};
+
+/**
+ *
+ *
+ * @param {*} app
+ * @param {*} ctx
+ * @param {*} options
+ * @param {*} body
+ * @returns
+ */
+const jsonRend = function (app, ctx, options, body) {
+    let contentType = 'application/json';
+    if (options.encoding !== false && contentType.indexOf('charset=') === -1) {
+        contentType = `${contentType}; charset=${options.encoding}`;
+    }
+    ctx.type = contentType;
+    if (ctx.status < 400) {
+        // body = (body || {});
+        if (lib.isJSONObj(body)) {
+            body.status = lib.isTrueEmpty(body.status) ? ctx.status : body.status;
+            body.message = body.message || ctx.message || '';
+            ctx.body = body;
+        } else {
+            ctx.body = {
+                'status': ctx.status,
+                'message': ctx.message || '',
+                'data': body
+            };
+        }
+        return null;
+    }
+    let message = ctx.message;
+    if (lib.isError(body)) {
+        message = body.message;
+    }
+    return ctx.res.end(`{"status": ${ctx.status},"message":"${message || ''}"}`);
+};
+
+/**
+ *
+ *
+ * @param {*} app
+ * @param {*} ctx
+ * @param {*} options
+ * @param {*} body
+ * @returns
+ */
+const textRend = function (app, ctx, options, body) {
+    let contentType = 'text/plain';
+    if (options.encoding !== false && contentType.indexOf('charset=') === -1) {
+        contentType = `${contentType}; charset=${options.encoding}`;
+    }
+    ctx.type = contentType;
+    if (ctx.status < 400) {
+        ctx.body = body || ' ';
+        return null;
+    }
+    let message = ctx.message;
+    if (lib.isError(body)) {
+        message = body.message;
+    }
+    return ctx.res.end(`Error: ${message || ''} `);
+};
+
+/**
+ *
+ *
+ * @param {*} app
+ * @param {*} ctx
+ * @param {*} options
+ * @param {*} body
+ * @returns {*} 
+ */
+const defaultRend = function (app, ctx, options, body) {
+    if (ctx.status < 400) {
+        ctx.body = body || ' ';
+        return null;
+    }
+    let message = ctx.message;
+    if (lib.isError(body)) {
+        message = body.message;
+    }
+    return ctx.res.end(`Error: ${message || ''} `);
+};
+
+/**
+ *
+ *
+ * @param {*} app
+ * @param {*} ctx
+ * @param {*} options
+ * @param {*} body
+ * @returns
+ */
+const responseBody = async function (app, ctx, options, body) {
+    try {
+        const contentType = parseResContentType(ctx);
+        // accepted types
+        switch (contentType) {
+            case 'json':
+                await jsonRend(app, ctx, options, body);
+                break;
+            case 'html':
+                await htmlRend(app, ctx, options, body);
+                break;
+            case 'text':
+                await textRend(app, ctx, options, body);
+                break;
+            default:
+                await defaultRend(app, ctx, options, body);
+                break;
+        }
+    } catch (err) {
+        logger.error(err);
+    }
+    return null;
+};
+
+/**
+ * parse response content-type
+ *
+ * @param {*} ctx
+ * @returns {*} 
+ */
+const parseResContentType = function (ctx) {
+    if (ctx.response.type === '') {
+        return ctx.accepts('json', 'html', 'text');
+    }
+    let type = ctx.response.is('json', 'html', 'text');
+    if (type) {
+        return type;
+    }
+    return '';
 };
 
 /**
@@ -41,40 +202,9 @@ const simpleRend = async function (ctx, options, err) {
  */
 const catcher = async function (app, ctx, options, err) {
     if (!app.isPrevent(err)) {
-        ctx.status = typeof err.status === 'number' ? err.status : (options.error_code || 500);
-        // accepted types
-        switch (ctx.accepts('json', 'html', 'text')) {
-            case 'json':
-                ctx.type = 'application/json';
-                ctx.res.end(`{"status": 0,"${options.error_no_key || 'code'}": ${ctx.status},"${options.error_msg_key || 'message'}":"${err.message || ''}"}`);
-                break;
-            case 'html':
-                ctx.type = 'text/html';
-                let body = '';
-                if (options.error_path) {
-                    body = await simpleRend(ctx, options, err).catch(e => '');
-                }
-                if (!body) {
-                    body = `<!DOCTYPE html><html><head><title>Error - ${ctx.status}</title><meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0">
-            <style>body {padding: 50px 80px;font: 14px 'Microsoft YaHei','微软雅黑',Helvetica,Sans-serif;}h1, h2 {margin: 0;padding: 10px 0;}h1 {font-size: 2em;}h2 {font-size: 1.2em;font-weight: 200;color: #aaa;}pre {font-size: .8em;}</style>
-            </head><body><div id="error"><h1>Error</h1><p>Oops! Your visit is rejected!</p>`;
-                    // if (app.app_debug || err.expose) {
-                    if (app.app_debug) {
-                        body = `${body}<h2>Message:</h2><pre><code>${err.message || ''}</code></pre><h2>Stack:</h2><pre><code>${err.stack || ''}</code></pre>`;
-                    }
-                    body = `${body}</div></body></html>`;
-                }
-
-                ctx.res.end(body);
-                break;
-            case 'text':
-            default:
-                ctx.type = 'text/plain';
-                ctx.res.end(`Error: ${err.message || ''} `);
-                break;
-        }
         app.emit('error', err, ctx);
-        return app.prevent();
+        ctx.status = (err && typeof err.status === 'number') ? err.status : (options.error_code || 500);
+        return responseBody(app, ctx, options, err);
     }
     return null;
 };
@@ -88,8 +218,10 @@ const catcher = async function (app, ctx, options, err) {
  */
 const timer = function (tmr, timeout) {
     return new Promise((resolve, reject) => {
-        /*eslint-disable no-return-assign */
-        return tmr = setTimeout(reject, timeout, httpError(408));
+        const err = new Error('Request Timeout');
+        err.status = 408;
+        tmr = setTimeout(reject, timeout, err);
+        return tmr;
     });
 };
 
@@ -97,15 +229,22 @@ const timer = function (tmr, timeout) {
  * default options
  */
 const defaultOptions = {
-    timeout: 30, //http请求超时时间,单位s
+    timeout: 10, //http服务超时时间,单位s
     error_code: 500, //报错时的状态码
-    error_no_key: 'code', //错误号的key
-    error_msg_key: 'message', //错误消息的key
+    error_key: 'code', //错误码的key
+    error_msg: 'message', //错误消息的key
     error_path: '', //错误模板目录配置.该目录下放置404.html、502.html等,框架会自动根据status进行渲染(支持模板变量,依赖think_view中间件;如果think_view中间件未加载,仅输出模板内容)
 };
 
 module.exports = function (options, app) {
     options = options ? lib.extend(defaultOptions, options, true) : defaultOptions;
+
+    if (options.error_path && (options.error_path).startsWith('./')) {
+        options.error_path = (options.error_path).replace('./', `${process.env.ROOT_PATH}/`);
+    }
+    // ms
+    options.timeout = (options.timeout || 30) * 1000;
+    options.encoding = app.config('encoding') || 'utf-8';
 
     let tmr;
     return async function (ctx, next) {
@@ -122,33 +261,28 @@ module.exports = function (options, app) {
 
         // response finish
         ctx.res.once('finish', function () {
-            //
-            let times = (Date.now() - ctx.startTime) || 0,
-                // style = '\x1B[32m', 
-                method = 'success';
-            if (ctx.status >= 400) {
-                // style = '\x1B[31m';
-                method = 'error';
+            let times = '';
+            if (app.app_debug) {
+                times = `${(Date.now() - ctx.startTime) || 0} ms`;
             }
-            logger[method](` ${ctx.method.toUpperCase()} ${ctx.status} ${ctx.originalPath || '/'} ${times} ms`);
-            // console[method](` ${ style } ${ ctx.method.toUpperCase() } ${ ctx.status } ${ ctx.originalPath || '/' } ${ times } ms\x1B[39m`);
+            logger[(ctx.status >= 400 ? 'error' : 'success')](` ${ctx.method} ${ctx.status} ${ctx.originalPath || '/'} ${times}`);
+            ctx = null;
         });
 
         // try /catch
-        tmr = null;
         try {
-            const timeout = (options.timeout || 30) * 1000;
             // promise.race
-            await Promise.race([timer(tmr, timeout), next()]);
+            const res = await Promise.race([timer(tmr, options.timeout), next()]);
             //404 error
-            if (ctx.status >= 400) {
-                ctx.throw(ctx.status, ctx.message);
+            if (res !== undefined && ctx.status === 404) {
+                ctx.status = 200;
             }
-            return null;
+            return responseBody(app, ctx, options, res);
         } catch (err) {
             return catcher(app, ctx, options, err);
         } finally {
             tmr && clearTimeout(tmr);
+            tmr = null;
         }
     };
 };
